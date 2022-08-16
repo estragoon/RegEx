d6df993f03ef510e7278057ee1b08cc681edeb8b
estragoon <simon.fm.charbonneau@gmail.com>
2022-08-16 23:03:27 +0200

    Add greedy function

--- regex.py
+++ regex.py
@@ -1,5 +1,3 @@
-from math import ceil
-
 def main():
 
     print(regex('aa', 'a'))
@@ -16,56 +14,73 @@ def regex(input, pattern):
 
     buffer = ''
 
-    etoile_index = -1
-
     etoile_buffer = ''
     
     etoile_input = input
 
-    pattern_buffer = pattern
-
     for i in range(len(pattern)):
 
-        # Detect etoile in pattern
-        if pattern[i] == '*':
-
-            # Put in etoile_buffer the text before the etoile
-            etoile_buffer = pattern[etoile_index + 1 : i]
-
-            etoile_index = i
-
-            # Match input == i * etoile_buffer (for maximum number of multiple for etoile_buffer)
-            for j in range(ceil(len(etoile_input) / len(etoile_buffer))):
-
-                dot_buffer = list(etoile_buffer)
-
-                # Replace each '.' by corresponding char in input
-                for o in range(len(dot_buffer)):
-
-                    if dot_buffer[o] == '.':
-
-                        try:
-                            dot_buffer[o] = etoile_input[o]
-                        
-                        except IndexError:
-                            return False
-
-                dot_buffer = ''.join(dot_buffer)
-
-                if etoile_input[:len(dot_buffer)] == dot_buffer:
-
-                    buffer += dot_buffer
-                    
-                    etoile_input = etoile_input[len(dot_buffer):]
-
-                else:
-
-                    break
+        if '*' not in pattern:
         
-            pattern_buffer = pattern_buffer[len(etoile_buffer) + 1 :]
-                    
-    buffer += pattern_buffer
+            break
 
+        else:
+        
+            # Detect etoile in pattern
+            if pattern[1] == '*':
+
+                # Put in etoile_buffer the text before the etoile
+                etoile_buffer = pattern[0]
+
+                # Match input == i * etoile_buffer (for maximum number of multiple for etoile_buffer)
+                for j in range(len(etoile_input)):
+
+                    # Replace '.' by corresponding char in input
+                    if etoile_buffer == '.':
+
+                        dot_buffer = etoile_input[0]
+
+                    else:
+
+                        dot_buffer = etoile_buffer
+                    
+                    # If match, add to buffer and cut etoile_input
+                    if etoile_input[0] == dot_buffer:
+
+                        buffer += dot_buffer
+                        
+                        etoile_input = etoile_input[1:]
+
+                    else:
+
+                        break
+            
+                # Cut etoile group from pattern_buffer
+                pattern = pattern[2:]
+
+            else:
+
+                buffer += pattern[0]
+                
+                etoile_input = etoile_input[1:]
+
+                pattern = pattern[1:]
+
+    for z in range(len(pattern)):
+
+        if pattern[z] == '.':
+
+            try:
+                buffer += etoile_input[z]
+
+            except IndexError:
+
+                return False
+
+        else:
+
+            buffer += pattern[z]
+    
     # Match for buffer == pattern
     if buffer == input:
 
