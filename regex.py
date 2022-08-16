864b542860690fde501ddcb860e84ea0f91d7405
estragoon <simon.fm.charbonneau@gmail.com>
2022-08-16 18:37:40 +0200

    Add etoile function for one etoile

--- regex.py
+++ regex.py
@@ -1,9 +1,11 @@
+from math import ceil
+
 
 def main():
 
-    regex('aa', 'a')
-    regex('aa', 'a*')
-    regex('ab', '.*')
+    print(regex('aa', 'a'))
+    print(regex('aaaaa', 'a*'))
+    print(regex('ab', '.*'))
 
 def regex(input, pattern):
     
@@ -11,11 +13,30 @@ def regex(input, pattern):
     
     for i in range(len(pattern)):
 
+        # Detect etoile in pattern
         if pattern[i] == '*':
 
+            # Put in buffer the text before the etoile
             buffer = pattern[:i]
 
-            print(buffer)
+            # Get maximum number of multiple for buffer
+            x = ceil(len(input) / len(buffer))
+
+            # Match input == i * buffer
+            for j in range(0, x + 1):
+
+                if input == buffer * j:
+
+                    return True
+
+            return False
+
+    # If no etoile match for input == pattern
+    if input == pattern:
+
+        return True
+
+    return False
 
 
 if __name__ == '__main__':
