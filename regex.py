540b312200d30806cf15ee616764c7a32793c893
estragoon <simon.fm.charbonneau@gmail.com>
2022-08-16 19:45:34 +0200

    Add function for dot

--- regex.py
+++ regex.py
@@ -6,31 +6,48 @@ def main():
     print(regex('aa', 'a*'))
     print(regex('ab', '.*'))
 
+    print(regex('abacada', 'a.*'))
+
 def regex(input, pattern):
     
-    buffer = ''
+    etoile_buffer = ''
     
     for i in range(len(pattern)):
 
         # Detect etoile in pattern
         if pattern[i] == '*':
 
-            # Put in buffer the text before the etoile
-            buffer = pattern[:i]
+            # Put in etoile_buffer the text before the etoile
+            etoile_buffer = pattern[:i]
 
-            # Get maximum number of multiple for buffer
-            x = ceil(len(input) / len(buffer))
+            # Get maximum number of multiple for etoile_buffer
+            x = ceil(len(input) / len(etoile_buffer))
 
-            # Match input == i * buffer
+            # Match input == i * etoile_buffer
             for j in range(0, x + 1):
 
-                if input == buffer * j:
+                dot_buffer = list(etoile_buffer * j)
+
+                # Replace each '.' by corresponding char in input
+                for i in range(len(dot_buffer)):
+
+                    if dot_buffer[i] == '.':
+
+                        try:
+                            dot_buffer[i] = input[i]
+                        
+                        except IndexError:
+                            return False
+
+                dot_buffer = ''.join(dot_buffer)
+
+                if input == dot_buffer:
 
                     return True
 
             return False
 
-    # If no etoile match for input == pattern
+    # If no etoile, match for input == pattern
     if input == pattern:
 
         return True
