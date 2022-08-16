233b1541d968bf6515d7aef4f437f876efc2e753
estragoon <simon.fm.charbonneau@gmail.com>
2022-08-17 00:29:25 +0200

    Add recursive file

---
+++ regex.py
@@ -0,0 +1,18 @@
+
+class regex(object):
+
+    def valid(self, input, pattern):
+
+        if not pattern:
+        
+            return not input
+
+        initial = bool(input) and pattern[0] in {input[0], '.'}
+
+        if len(pattern) > 1 and pattern[1] == '*':
+            
+            return (self.valid(input, pattern[2:]) or initial and self.valid(input[1:], pattern))
+        
+        else:
+        
+            return initial and self.valid(input[1:], pattern[1:])
